741cecbab970d41059f8f2b1e7fc0c929e8fad12
guru2024 <130414012+guru2024@users.noreply.github.com>
2023-06-22 16:30:09 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,24 +1,7 @@
 from app import change
 
-def change(amount):
-    quarters = amount // 0.25
-    nickels = (amount - quarters * 0.25) // 0.05
-    pennies = (amount - quarters * 0.25 - nickels * 0.05) // 0.01
-    return [
-        {'quantity': quarters, 'coin': 'quarters'},
-        {'quantity': nickels, 'coin': 'nickels'},
-        {'quantity': pennies, 'coin': 'pennies'},
-    ]
-
 def test_change():
-    quarters = 5
-    nickels = 1
-    pennies = 4
-    expected_result = [
-        {'quantity': quarters, 'coin': 'quarters'},
-        {'quantity': nickels, 'coin': 'nickels'},
-        {'quantity': pennies, 'coin': 'pennies'},
-    ]
+    expected_result = [{5: 'quarters'}, {1: 'nickels'}, {4: 'pennies'}]
     assert expected_result == change(1.34)
 
 if __name__ == '__main__':
