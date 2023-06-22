f1430b963163db756b55cc0c8617a5fc04045caa
guru2024 <130414012+guru2024@users.noreply.github.com>
2023-06-22 16:32:29 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,8 +1,10 @@
 from app import change
 
+import pytest
+
 def test_change():
     expected_result = [{5: 'quarters'}, {1: 'nickels'}, {4: 'pennies'}]
     assert expected_result == change(1.34)
 
 if __name__ == '__main__':
-    test_change()
+    pytest.main()
